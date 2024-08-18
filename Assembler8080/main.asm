98838657b76afc5f211be3be549c0fab6358face
Eduardo Acauan <Eduardo Acauan@192.168.15.201>
2024-08-18 00:41:11 -0300

    8080 ASM

--- Assembler8080/main.asm
+++ Assembler8080/main.asm
@@ -1,2 +1,11 @@
-	ORG	0x0000
-	MVI	B, 6
+/*******************
+* Programa Teste   *
+* Autor: Eduardo A *
+********************/
+         ORG   0x8000 //endereco 0x8000
+         MVI   A, 'a'
+         ANI   0xDF
+         LXI   H, 3 * 3
+         MOV   (HL), A // move o conteudo do registrador A para o endereço apontado por HL. (tambem pode ser escrito como MOV M, A)
+         RET
+; fim
